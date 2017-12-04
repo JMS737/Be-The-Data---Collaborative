a77f8d5be084bbb1a992c7edad3e81ce8e05aa79
Jeremy Sapsed <jms737800@gmail.com>
2017-12-04 15:24:57 +0000

    Added Axis

---
+++ Assets/Prefabs/Graphs/Axes.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DataVis.Collaboration
+{
+    public class Axes : MonoBehaviour
+    {
+
+        public int xSize = 5;
+        public int ySize = 5;
+        public int zSize = 5;
+
+        private GameObject xAxis;
+        private GameObject yAxis;
+        private GameObject zAxis_left;
+        private GameObject zAxis_right;
+
+        // Renders the grids to the parameterised dimensions
+        public void renderGrid(int x, int y, int z)
+        {
+
+            // Set local sizes
+            this.xSize = x;
+            this.ySize = y;
+            this.zSize = z;
+
+            // Set the grid's gameobjects to manipulate
+            xAxis = this.gameObject.transform.GetChild(0).gameObject;
+            yAxis = this.gameObject.transform.GetChild(1).gameObject;
+            zAxis_left = this.gameObject.transform.GetChild(2).gameObject;
+            zAxis_right = this.gameObject.transform.GetChild(3).gameObject;
+
+            // Scale each grid to the relevant dimensions
+            xAxis.transform.localScale = new Vector3((float)xSize, (float)zSize, 0.01f);
+            yAxis.transform.localScale = new Vector3((float)xSize, (float)ySize, 0.01f);
+            zAxis_left.transform.localScale = new Vector3((float)zSize, (float)ySize, 0.01f);
+            zAxis_right.transform.localScale = new Vector3((float)zSize, (float)ySize, 0.01f);
+
+            // Position the grids
+            if ((xSize == ySize) && (ySize == zSize))
+            {
+                // X, Y, Z components are the same
+                xAxis.transform.position = new Vector3(xSize / 2.0f, 0.0f, xSize / 2.0f);
+                yAxis.transform.position = new Vector3(ySize / 2.0f, ySize / 2.0f, ySize);
+                zAxis_left.transform.position = new Vector3(0f, zSize / 2.0f, zSize / 2.0f);
+                zAxis_right.transform.position = new Vector3(zSize, zSize / 2.0f, zSize / 2.0f);
+            }
+            else
+            {
+                // At least one component is different, so position differently
+                xAxis.transform.position = new Vector3(xSize / 2.0f, 0.0f, zSize / 2.0f);
+                yAxis.transform.position = new Vector3(xSize / 2.0f, ySize / 2.0f, zSize);
+                zAxis_left.transform.position = new Vector3(0f, ySize / 2.0f, zSize / 2.0f);
+                zAxis_right.transform.position = new Vector3(xSize, ySize / 2.0f, zSize / 2.0f);
+            }
+
+            // Adjust textures to spread evenly over the passed dimensions
+            xAxis.GetComponent<Renderer>().material.mainTextureScale = new Vector2(xSize, zSize);
+            yAxis.GetComponent<Renderer>().material.mainTextureScale = new Vector2(xSize, ySize);
+            zAxis_left.GetComponent<Renderer>().material.mainTextureScale = new Vector2(zSize, ySize);
+            zAxis_right.GetComponent<Renderer>().material.mainTextureScale = new Vector2(zSize, ySize);
+        }
+    }
+}
+
+
---
+++ Assets/Prefabs/Graphs/GraphManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataVis.Collaboration
+{
+    public class GraphManager : MonoBehaviour
+    {
+
+        private Axes axes;
+
+    // Use this for initialization
+        void Start()
+        {
+            axes = GetComponentInChildren<Axes>();
+            axes.renderGrid(10, 10, 10);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
