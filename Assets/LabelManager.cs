c5d54cfe7cae2a24d55f1755536e8d7daf5cb3a1
Jeremy Sapsed <jms737800@gmail.com>
2017-12-07 00:03:07 +0000

    Added names to axis labels

--- Assets/LabelManager.cs
+++ Assets/LabelManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LabelManager : MonoBehaviour {
 
+    public string xName;
+    public string yName;
+    public string zName;
+
     private GameObject xAxisLabel;
     private GameObject yLAxisLabel;
     private GameObject yRAxisLabel;
@@ -17,6 +21,12 @@ public class LabelManager : MonoBehaviour {
         yRAxisLabel = transform.GetChild(2).gameObject;
         zLAxisLabel = transform.GetChild(3).gameObject;
         zRAxisLabel = transform.GetChild(4).gameObject;
+
+        xAxisLabel.GetComponent<TextMesh>().text = xName;
+        yLAxisLabel.GetComponent<TextMesh>().text = yName;
+        yRAxisLabel.GetComponent<TextMesh>().text = yName;
+        zLAxisLabel.GetComponent<TextMesh>().text = zName;
+        zRAxisLabel.GetComponent<TextMesh>().text = zName;
     }
 
     public void SetPositions(float maxX, float maxY, float maxZ)
