35d6f9eacf03a9d3dd8a48c1ad4361256497daef
Jeremy Sapsed <jms737800@gmail.com>
2018-02-09 11:25:29 +0000

    Changed colour allocaton to use RPCs
    Player colours have been moved to the PlayerManager script and now use RPCs to unsure colours are set correctly on all clients.
    This also fixes the issue colours being out of sync when clients disconnect and reconnect.

--- Assets/PlayerHead.cs
+++ Assets/PlayerHead.cs
@@ -6,31 +6,31 @@ namespace DataVis.Collaboration
 {
 	public class PlayerHead : MonoBehaviour {
 
-		public static List<Color> playerColours = new List<Color>
-		{
-			Color.blue,
-			Color.yellow,
-			Color.magenta,
-			Color.white,
-		};
+		//public static List<Color> playerColours = new List<Color>
+		//{
+		//	Color.blue,
+		//	Color.yellow,
+		//	Color.magenta,
+		//	Color.white,
+		//};
 
-		private static int colourIndex = -1;
+		//private static int colourIndex = -1;
 
-		public static int ColourIndex {
-			get {
-				colourIndex++;
-				if (colourIndex >= playerColours.Count) {
-					colourIndex = 0;
-				}
-				return colourIndex;
-			}
-		}
+		//public static int ColourIndex {
+		//	get {
+		//		colourIndex++;
+		//		if (colourIndex >= playerColours.Count) {
+		//			colourIndex = 0;
+		//		}
+		//		return colourIndex;
+		//	}
+		//}
 
-		// Use this for initialization
-		void Start () {
+		//// Use this for initialization
+		//void Start () {
 
-			Material material = GetComponent<Renderer> ().material;
-			material.color = playerColours [ColourIndex];
-		}
+		//	Material material = GetComponent<Renderer> ().material;
+		//	material.color = playerColours [ColourIndex];
+		//}
 	}
 }
--- Assets/Prefabs/Player/PlayerManager.cs
+++ Assets/Prefabs/Player/PlayerManager.cs
@@ -8,8 +8,28 @@ namespace DataVis.Collaboration
     {
 
         public static GameObject LocalPlayerInstance;
-
         public GameObject Laser;
+
+        public GameObject head;
+
+        public List<Color> playerColours;
+
+        private static int colourIndex = -1;
+
+        public static int ColourIndex
+        {
+            get
+            {
+                colourIndex++;
+                if (colourIndex >= 4)
+                {
+                    colourIndex = 0;
+                }
+                return colourIndex;
+            }
+        }
+
+        private Color playerColour = Color.white;
 
 		// TODO: Use RPCs to add/remove highlights (e.g. specify a position and a colour)
 		// TODO: Look at changing colour selection to use RPCs
@@ -45,8 +65,21 @@ namespace DataVis.Collaboration
 
 				Laser.SetActive(true);
 
-//				playerColour = playerColours [ColourIndex];
+                photonView.RPC("SetColour", PhotonTargets.AllBufferedViaServer, ColourIndex);
             }
+        }
+
+        [PunRPC]
+        public void SetColour(int colourIx)
+        {
+            Debug.Log("My player colour is: " + playerColour);
+
+            // Set the value so buffered RPC calls from existing clients will
+            // be accounted for.
+            colourIndex = colourIx;
+
+            playerColour = playerColours[colourIx];
+            head.GetComponent<Renderer>().material.color = playerColour;
         }
     }
 }
